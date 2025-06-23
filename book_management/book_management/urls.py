4a5b4e30f130d98a552bad0ae116692729eb7d53
MehediAlam49 <mehedialam806@gmail.com>
2025-06-24 01:13:59 +0600

    complete curd and authentication

--- book_management/book_management/urls.py
+++ book_management/book_management/urls.py
@@ -3,6 +3,9 @@ from django.contrib import admin
 from django.urls import path
 from book.views import *
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('registerPage/', registerPage, name='registerPage'),
@@ -13,7 +16,7 @@ urlpatterns = [
 
     path('', home, name='home'),
     path('addBook/', addBook, name='addBook'),
-    path('editBook/', editBook, name='editBook'),
-    path('viewBook/', viewBook, name='viewBook'),
-    path('deleteBook/', deleteBook, name='deleteBook'),
-]
+    path('editBook/<str:id>', editBook, name='editBook'),
+    path('viewBook/<str:id>', viewBook, name='viewBook'),
+    path('deleteBook/<str:id>', deleteBook, name='deleteBook'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
